d6a85ab8237836db18eed35470ec3f6f3a72286a
Marc Magon <ged_marc@hotmail.com>
2017-03-04 09:24:25 +0200

    v0.9

--- src/za/co/mmagon/jwebswing/plugins/jqueryui/JQUIPageConfigurator.java
+++ src/za/co/mmagon/jwebswing/plugins/jqueryui/JQUIPageConfigurator.java
@@ -1,3 +1,19 @@
+/* 
+ * Copyright (C) 2017 Marc Magon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package za.co.mmagon.jwebswing.plugins.jqueryui;
 
 import java.util.logging.Logger;
@@ -14,16 +30,22 @@ import za.co.mmagon.logger.LogFactory;
  * @since 13 Feb 2017
  *
  */
-@PluginInformation(pluginName = "JQuery UI", pluginUniqueName = "jquery-ui",
-        pluginDescription = "jQuery UI is a curated set of user interface interactions, effects, widgets, and themes built on top of the jQuery JavaScript Library. Whether you're building highly interactive web applications or you just need to add a date picker to a form control, jQuery UI is the perfect choice.",
-        pluginVersion = "1.12.1",
-        pluginDependancyUniqueIDs = "",
-        pluginCategories = "jquery, ui, jquery-ui",
-        pluginSubtitle = "A complete UI framework built using JQuery",
-        pluginGitUrl = "https://github.com/GedMarc/JWebSwing-JQueryUIPlugin",
-        pluginSourceUrl = "http://jqueryui.com/",
-        pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-JQueryUIPlugin/wiki",
-        pluginOriginalHomepage = "http://jqueryui.com/")
+@PluginInformation(pluginName = "JQuery UI",
+                   pluginUniqueName = "jquery-ui",
+                   pluginDescription = "jQuery UI is a curated set of user interface interactions, effects, widgets, and themes built on top of the jQuery JavaScript Library. Whether you're building highly interactive web applications or you just need to add a date picker to a form control, jQuery UI is the perfect choice.",
+                   pluginVersion = "1.12.1",
+                   pluginDependancyUniqueIDs = "jquery",
+                   pluginCategories = "jquery, ui, jquery-ui,themes, styles, styling",
+                   pluginSubtitle = "A complete UI framework built using JQuery",
+                   pluginGitUrl = "https://github.com/GedMarc/JWebSwing-JQueryUIPlugin",
+                   pluginSourceUrl = "http://jqueryui.com/",
+                   pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-JQueryUIPlugin/wiki",
+                   pluginOriginalHomepage = "http://jqueryui.com/",
+                   pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/JQueryUIPlugin.jar/download",
+                   pluginIconUrl = "bower_components/jquery-ui/jqueryui_icon.ico",
+                   pluginIconImageUrl = "bower_components/jquery-ui/jqueryui_example.jpg",
+                   pluginLastUpdatedDate = "2017/03/04"
+)
 public class JQUIPageConfigurator extends PageConfigurator
 {
 
